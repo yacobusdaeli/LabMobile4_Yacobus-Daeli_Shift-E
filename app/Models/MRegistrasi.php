ba7d749939350ca53e09fe771a56945fa45176b8
s0crates_ <aldebaranyac@gmail.com>
2024-10-03 08:21:06 +0700

    Upload file sekaligus screenshoot

---
+++ app/Models/MRegistrasi.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MRegistrasi extends Model
+{
+    protected $table = 'member';
+    protected $allowedFields = ['nama', 'email', 'password'];
+}
+?>
